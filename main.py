8bb18cad49706b8cd9d57443e9cbbe78a5cc29b5
dennodk <dennodk@hotmail.com>
2021-10-05 10:39:18 +0200

    test

---
+++ main.py
@@ -0,0 +1,11 @@
+import sys
+from PyQt5.QtWidgets import QApplication
+from View import MainWindow
+from Model import Model
+
+model = Model()
+model.load()
+app = QApplication(sys.argv)
+mainwindow = MainWindow(model)
+mainwindow.show()
+app.exec()
